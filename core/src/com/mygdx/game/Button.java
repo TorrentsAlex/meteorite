99a4d70b834aa951f239c37cade33fda625f9239
alex torrents <torrents.alex1@gmail.com>
2015-12-26 23:36:47 +0100

    Update last scene

---
+++ core/src/com/mygdx/game/Button.java
@@ -0,0 +1,24 @@
+package com.mygdx.game;
+
+import com.badlogic.ashley.core.Entity;
+import com.uwsoft.editor.renderer.scripts.IScript;
+
+/**
+ * Created by Alex Torrents on 22/12/2015.
+ */
+public class Button implements IScript {
+    @Override
+    public void init(Entity entity) {
+
+    }
+
+    @Override
+    public void act(float delta) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
